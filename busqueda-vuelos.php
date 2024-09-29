47934a5a625ad0df90e8a4d111181c533febbc8e
ValeskaSobarzo <valeskasobarzo8@gmail.com>
2024-09-29 08:50:24 -0300

    subir

--- busqueda-vuelos.php
+++ busqueda-vuelos.php
@@ -1,1 +1,11 @@
+<?php
+// Función básica de búsqueda de vuelos
 
+function buscarVuelos($origen, $destino, $fechaSalida, $fechaRegreso) {
+    // Llamada a un servicio externo de reservas de vuelos
+    $apiUrl = "https://api.ejemplo.com/vuelos?origen=$origen&destino=$destino&salida=$fechaSalida&regreso=$fechaRegreso";
+    // Obtener datos de la API
+    $response = file_get_contents($apiUrl);
+    $vuelos = json_decode($response, true);
+    return $vuelos;
+}
